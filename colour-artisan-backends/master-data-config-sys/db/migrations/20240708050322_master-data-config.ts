4a9f2c790254543ec117b72b4bf7b180109e5344
Barnista <developer@theintegrator.co.th>
2024-07-08 14:19:45 +0700

    commit migrate uuid example

--- colour-artisan-backends/master-data-config-sys/db/migrations/20240708050322_master-data-config.ts
+++ colour-artisan-backends/master-data-config-sys/db/migrations/20240708050322_master-data-config.ts
@@ -1,118 +1,130 @@
 import type { Knex } from "knex";
 import { TABLE_NAMES } from "../db";
+import { IDGenerator } from "../../src/services/id_generator";
 
 
 export async function up(knex: Knex): Promise<void> {
     return knex.schema
-        .alterTable(TABLE_NAMES.DBVersions, function(table){
+        .alterTable(TABLE_NAMES.DBVersions, function (table) {
+            table.dropColumn('id');
+            table.string('id', 10).primary({
+                constraintName: `${TABLE_NAMES.DBVersions}_primary_key`,
+                deferrable: 'deferred',
+            })
+            .notNullable()
+            .defaultTo(IDGenerator.newUUID());
+
             table.timestamp('deleted_at');
             table.string('deleted_by', 10);
         })
-        .alterTable(TABLE_NAMES.CanSizes, function(table){
+        .alterTable(TABLE_NAMES.CanSizes, function (table) {
             table.timestamp('deleted_at');
             table.string('deleted_by', 10);
         })
-        .alterTable(TABLE_NAMES.CanUnits, function(table){
+        .alterTable(TABLE_NAMES.CanUnits, function (table) {
             table.timestamp('deleted_at');
             table.string('deleted_by', 10);
         })
-        .alterTable(TABLE_NAMES.ProductCanSizes, function(table){
+        .alterTable(TABLE_NAMES.ProductCanSizes, function (table) {
             table.timestamp('deleted_at');
             table.string('deleted_by', 10);
         })
-        .alterTable(TABLE_NAMES.ProductGroups, function(table){
+        .alterTable(TABLE_NAMES.ProductGroups, function (table) {
             table.timestamp('deleted_at');
             table.string('deleted_by', 10);
         })
-        .alterTable(TABLE_NAMES.Products, function(table){
+        .alterTable(TABLE_NAMES.Products, function (table) {
             table.timestamp('deleted_at');
             table.string('deleted_by', 10);
         })
-        .alterTable(TABLE_NAMES.ProductBases, function(table){
+        .alterTable(TABLE_NAMES.ProductBases, function (table) {
             table.timestamp('deleted_at');
             table.string('deleted_by', 10);
         })
-        .alterTable(TABLE_NAMES.SubProducts, function(table){
+        .alterTable(TABLE_NAMES.SubProducts, function (table) {
             table.timestamp('deleted_at');
             table.string('deleted_by', 10);
         })
-        .alterTable(TABLE_NAMES.ProductShadeCodes, function(table){
+        .alterTable(TABLE_NAMES.ProductShadeCodes, function (table) {
             table.timestamp('deleted_at');
             table.string('deleted_by', 10);
         })
-        .alterTable(TABLE_NAMES.ProductTinters, function(table){
+        .alterTable(TABLE_NAMES.ProductTinters, function (table) {
             table.timestamp('deleted_at');
             table.string('deleted_by', 10);
         })
-        .alterTable(TABLE_NAMES.GeneralPricings, function(table){
+        .alterTable(TABLE_NAMES.GeneralPricings, function (table) {
             table.timestamp('deleted_at');
             table.string('deleted_by', 10);
         })
-        .alterTable(TABLE_NAMES.ProductBasePricings, function(table){
+        .alterTable(TABLE_NAMES.ProductBasePricings, function (table) {
             table.timestamp('deleted_at');
             table.string('deleted_by', 10);
         })
-        .alterTable(TABLE_NAMES.TinterPricings, function(table){
+        .alterTable(TABLE_NAMES.TinterPricings, function (table) {
             table.timestamp('deleted_at');
             table.string('deleted_by', 10);
         })
-        //NEW REQUIREMENT ---> add deleted_at and deleted_by to every table
-        //NEW REQUIREMENT ---> change ID column to custom UUID
-        //ref: https://www.npmjs.com/package/custom-uuid
+    //NEW REQUIREMENT ---> add deleted_at and deleted_by to every table
+    //NEW REQUIREMENT ---> change ID column to custom UUID
+    //ref: https://www.npmjs.com/package/custom-uuid
 }
 
 
 export async function down(knex: Knex): Promise<void> {
     return knex.schema
-        .alterTable(TABLE_NAMES.DBVersions, function(table){
+        .alterTable(TABLE_NAMES.DBVersions, function (table) {
+            table.dropColumn('id');
+            table.increments('id');
+
             table.dropColumn('deleted_at');
             table.dropColumn('deleted_by');
         })
-        .alterTable(TABLE_NAMES.CanSizes, function(table){
+        .alterTable(TABLE_NAMES.CanSizes, function (table) {
             table.dropColumn('deleted_at');
             table.dropColumn('deleted_by');
         })
-        .alterTable(TABLE_NAMES.CanUnits, function(table){
+        .alterTable(TABLE_NAMES.CanUnits, function (table) {
             table.dropColumn('deleted_at');
             table.dropColumn('deleted_by');
         })
-        .alterTable(TABLE_NAMES.ProductCanSizes, function(table){
+        .alterTable(TABLE_NAMES.ProductCanSizes, function (table) {
             table.dropColumn('deleted_at');
             table.dropColumn('deleted_by');
         })
-        .alterTable(TABLE_NAMES.ProductGroups, function(table){
+        .alterTable(TABLE_NAMES.ProductGroups, function (table) {
             table.dropColumn('deleted_at');
             table.dropColumn('deleted_by');
         })
-        .alterTable(TABLE_NAMES.Products, function(table){
+        .alterTable(TABLE_NAMES.Products, function (table) {
             table.dropColumn('deleted_at');
             table.dropColumn('deleted_by');
         })
-        .alterTable(TABLE_NAMES.ProductBases, function(table){
+        .alterTable(TABLE_NAMES.ProductBases, function (table) {
             table.dropColumn('deleted_at');
             table.dropColumn('deleted_by');
         })
-        .alterTable(TABLE_NAMES.SubProducts, function(table){
+        .alterTable(TABLE_NAMES.SubProducts, function (table) {
             table.dropColumn('deleted_at');
             table.dropColumn('deleted_by');
         })
-        .alterTable(TABLE_NAMES.ProductShadeCodes, function(table){
+        .alterTable(TABLE_NAMES.ProductShadeCodes, function (table) {
             table.dropColumn('deleted_at');
             table.dropColumn('deleted_by');
         })
-        .alterTable(TABLE_NAMES.ProductTinters, function(table){
+        .alterTable(TABLE_NAMES.ProductTinters, function (table) {
             table.dropColumn('deleted_at');
             table.dropColumn('deleted_by');
         })
-        .alterTable(TABLE_NAMES.GeneralPricings, function(table){
+        .alterTable(TABLE_NAMES.GeneralPricings, function (table) {
             table.dropColumn('deleted_at');
             table.dropColumn('deleted_by');
         })
-        .alterTable(TABLE_NAMES.ProductBasePricings, function(table){
+        .alterTable(TABLE_NAMES.ProductBasePricings, function (table) {
             table.dropColumn('deleted_at');
             table.dropColumn('deleted_by');
         })
-        .alterTable(TABLE_NAMES.TinterPricings, function(table){
+        .alterTable(TABLE_NAMES.TinterPricings, function (table) {
             table.dropColumn('deleted_at');
             table.dropColumn('deleted_by');
         })
